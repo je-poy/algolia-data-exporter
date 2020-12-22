b3aeb9d7646fc6ae5ebf297f7881983b3ac7fef1
je-poy <canalejeff@gmail.com>
2020-12-22 09:01:59 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Only in Node.js
+const fs = require('fs');
+const algoliasearch = require('algoliasearch');
+const moment = require("moment-timezone")
+
+moment.tz.setDefault("Asia/Manila");
+
+const client = algoliasearch(process.env.ALGOLIA_APPID, process.env.ALGOLIA_ADMINKEY);
+const index = client.initIndex(process.env.ALGOLIA_INDEX);
+
+let hits = [];
+
+index
+  .browseObjects({
+    batch: (objects) => (hits = hits.concat(objects)),
+  })
+  .then(() => {
+    console.log('Finished! We got %d hits', hits.length);
+    fs.writeFile(
+      `${process.env.EXTRACT_PATH}/${moment().format('M-D-yyyy')}-${process.env.ALGOLIA_INDEX}.json`,
+      JSON.stringify(hits, null, 2),
+      'utf-8',
+      (err) => {
+        if (err) throw err;
+        console.log('Your index was successfully exported!');
+      }
+    );
+  });
